6aa0fe8534c69ab3c293dfe7ace8a5efbd9f891e
darkdestroyeriii <yoozernam3@gmail.com>
2024-10-20 03:56:41 -0700

    Cleaned up the file a little and some other stuff.

--- ESP32-Calhacks/ESP32-Calhacks.ino
+++ ESP32-Calhacks/ESP32-Calhacks.ino
@@ -14,6 +14,15 @@ Adafruit_PWMServoDriver board = Adafruit_PWMServoDriver(0x40);
 #define shortPulse 900
 #define longPulse 2100
 
+#define minWalkHeight 1
+#define maxWalkHeight 12
+
+#define minStepHeight 12
+#define maxStepHeight 20
+
+#define minForward  6
+#define maxForward  30
+
 String readBluetoothData() {
   String data = "";
   while (SerialBT.available()) {
@@ -27,12 +36,14 @@ String readBluetoothData() {
 int servoArchMin = 90;
 
 int servoArchMax = 120;
+//sensorPin, hapticPin, board, minValue, maxValue, minDistance, maxDistance, direction
+SensorHaptic servoToe = SensorHaptic(2, 1, &board, 90, 120, minForward, maxForward, -1);
 
-SensorHaptic servoHeel = SensorHaptic(15, 0, &board, 90, 120, 0, 40, 1);
-SensorHaptic hapticHeel = SensorHaptic(15, 32, NULL, 135, 255, 40, 100, 1);
-SensorHaptic servoToe = SensorHaptic(2, 1, &board, 90, 120, 0, 50, -1);
-SensorHaptic servoArch = SensorHaptic(4, 2, &board, 90, 120, 0, 40, 1);
-SensorHaptic hapticArch = SensorHaptic(4, 33, NULL, 130, 255, 40, 200, 1);
+SensorHaptic servoArch = SensorHaptic(4, 2, &board, 90, 120, minWalkHeight, maxWalkHeight, 1);
+SensorHaptic servoHeel = SensorHaptic(15, 0, &board, 90, 120, minWalkHeight, maxWalkHeight, 1);
+
+SensorHaptic hapticArch = SensorHaptic(4, 33, NULL, 130, 255, minStepHeight, maxStepHeight, 1);
+SensorHaptic hapticHeel = SensorHaptic(15, 32, NULL, 135, 255, minStepHeight, maxStepHeight, 1);
 
 int testServo = 8;
 
@@ -74,9 +85,9 @@ void loop() {
         }
        }
      if(isNum){
-      Serial.println("This is a number fam"+ received);
+      //Serial.println("This is a number fam"+ received);
         if(received.length() > 0){
-          Serial.println("This is the length" + received.length());
+          //Serial.println("This is the length" + received.length());
         int angle = received.toInt();
         constrain(angle,0,180);
         int pulse = map(angle,0,180,SERVOMIN,SERVOMAX);
@@ -88,21 +99,22 @@ void loop() {
      }
     }
   }
-  // float volts = analogRead(2) * 0.0008056640625; // value from sensor * (3.3/4096)
-  // int distance_cm = 29.988 * pow( volts, -1.173);
-  // if (distance_cm < 90){
-  //   int ang = 0;
-  //   ang = map(ang, 0, 180, servoArchMin, servoArchMax);
-  //   int pulse = angleToPulse(ang);
-  //   board.setPWM(servoToe, 0, pulse);
-  // }
-//  servoHeel.update();
-//  servoToe.update();
-//  servoArch.update();
-//  hapticHeel.update(true);
-//  
-//  delay(100);  // Short delay to avoid flooding the serial output
-//}
-//int angleToPulse(int ang) {
-//  return map(ang, 0, 180, SERVOMIN, SERVOMAX); // Map angle (0-180) to pulse (SERVOMIN-SERVOMAX)
+  distance_cm = servoToe.readSensor();
+  if (distance_cm < 90){
+    int ang = 0;
+    ang = map(ang, 0, 180, servoArchMin, servoArchMax);
+    int pulse = angleToPulse(ang);
+    board.setPWM(servoToe, 0, pulse);
+  }
+
+ servoToe.update();
+ servoArch.update();
+ hapticArch.update(true);
+ servoHeel.update();
+ hapticHeel.update(true);
+ 
+ delay(100);  // Short delay to avoid flooding the serial output
+}
+int angleToPulse(int ang) {
+ return map(ang, 0, 180, SERVOMIN, SERVOMAX); // Map angle (0-180) to pulse (SERVOMIN-SERVOMAX)
 }
